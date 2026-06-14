54eacc6d2857a91024bac44005d6b515715f8ddf
tensaisendo <tensai.sendo@gmail.com>
2026-06-15 00:15:33 +0200

    Initial Strapi

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -4,7 +4,18 @@ const config: Core.Config.Middlewares = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'http://localhost:5500',
+        'http://127.0.0.1:5500',
+        'http://localhost:3000',
+        '*'
+      ],
+      headers: '*',
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
